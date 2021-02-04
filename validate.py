6ae998b0795e409128908191cd1e44c79e644ef2
Zain <emailzainkhan@gmail.com>
2021-02-04 17:31:28 +0000

    lxml dependancy has some issues. Changed to use xmlschema package which is also alot simplier

--- validate.py
+++ validate.py
@@ -1,6 +1,4 @@
-import markdown, sys
-from lxml import etree
-from io import StringIO
+import markdown, sys, xmlschema
 
 # File names as command line arguments
 filename_xsd = sys.argv[1]
@@ -20,43 +18,11 @@ xhtml = markdown.markdown(markdown_text)
 xhtml = "<root>\n" + xhtml + "\n</root>"
 
 
-""" # Write out html. Useful for debugging
+# Write out html. Useful for debugging
 with open("output.html", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
-    output_file.write(xhtml) """
-
-
-# Make sure we have valid xhtml
-try:
-    doc = etree.parse(StringIO(xhtml))
-    print('XML well formed, syntax ok.')
-
-except IOError:
-    print('Invalid File')
-
-except etree.XMLSyntaxError as err:
-    print('XML Syntax Error, see error_syntax.log')
-    with open('error_syntax.log', 'w') as error_log_file:
-        error_log_file.write(str(err.error_log))
-    quit()
-
-except:
-    print('Unknown error, exiting.')
-    quit()
+    output_file.write(xhtml)
 
 
 # Validate against schema
-try:
-    xmlschema_doc = etree.parse(StringIO(schema_to_check))
-    xmlschema = etree.XMLSchema(xmlschema_doc)
-    xmlschema.assertValid(doc)
-    print('XML valid, schema validation ok.')
-
-except etree.DocumentInvalid as err:
-    print('Schema validation error, see error_schema.log')
-    with open('error_schema.log', 'w') as error_log_file:
-        error_log_file.write(str(err.error_log))
-    quit()
-
-except:
-    print('Unknown error, exiting.')
-    quit()
+xmlschema.validate('output.html', filename_xsd)
+print('XML valid, schema validation ok.')
